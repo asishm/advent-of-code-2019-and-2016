b45aadebf0a4bffba43d7089efc109fcd608b4e6
Asish Mahapatra <asishkm@gmail.com>
2019-12-03 11:24:29 +0530

    complete day1 - concurrency

--- 2019/day1_upping.py
+++ 2019/day1_upping.py
@@ -5,3 +5,15 @@ def calc_fuel(x):
         if x > 0:
             f += x
     return f
+
+def parse(inp):
+    return list(map(int, inp.splitlines()))
+
+if __name__ == "__main__":
+    import sys
+    inp = parse(sys.stdin.read())
+
+    import multiprocessing as mp
+    with mp.Pool(processes=None) as pool:
+        results = pool.map(calc_fuel, inp)
+    print(sum(results))
