c01cfd479e0c5b502a12c17078e9c6aa6312d836
Asish Mahapatra <asishkm@gmail.com>
2019-12-06 11:49:04 +0530

    better networkx usage

--- 2019/day6.py
+++ 2019/day6.py
@@ -8,19 +8,11 @@ def parse(inp):
     return list(k.split(')') for k in inp.splitlines() if k.strip())
 
 def part1(inp, *args, **kwargs):
-    G = nx.DiGraph()
-    for a,b in inp:
-        G.add_edge(a, b)
-    root_node, *other_nodes = nx.topological_sort(G)
-    total = 0
-    for node in other_nodes:
-        total += nx.shortest_path_length(G, root_node, node)
-    return total
+    G = nx.from_edgelist(inp)
+    return sum(nx.shortest_path_length(G, 'COM').values())
 
 def part2(inp, *args, **kwargs):
-    G = nx.Graph()
-    for a,b in inp:
-        G.add_edge(a, b)
+    G = nx.from_edgelist(inp)
     return nx.shortest_path_length(G, 'SAN', 'YOU') - 2
     
 if __name__ == "__main__":
