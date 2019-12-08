75e39e8ffcbcfac1eadb30883ffc97359d765965
Asish Mahapatra <asishkm@gmail.com>
2019-12-09 01:31:24 +0530

    OCR + image generation

--- 2019/day8.py
+++ 2019/day8.py
@@ -1,4 +1,7 @@
 from collections import Counter
+from PIL import Image
+import subprocess as sp
+import os
 
 def parse(inp):
     return [int(k) for k in inp.strip()]
@@ -10,18 +13,39 @@ def part1(inp, *args, **kwargs):
 
 def part2(inp, *args, **kwargs):
     ## ugly one-liner
-    ## return '\n'.join([''.join([next('■■ ' if val == 1 else '   ' for val in inp[i*25+j:len(inp):25*6] if val !=2) for j in range(25)]) for i in range(6)])
+    # return '\n'.join(''.join(next('\u2588' if val == 1 else ' ' for val in inp[i*25+j:len(inp):25*6] if val !=2) for j in range(25)) for i in range(6))
     grid = [[inp[i*25+j:len(inp):25*6] for j in range(25)] for i in range(6)]
     out = []
+    width = 50
+    height = 12
+    margin_width = 3
+    margins = [(255,255,255)] * (margin_width * 2 + width)
+    for _ in range(margin_width):
+        out.append(margins)
     for row in grid:
-        tmp = []
+        tmp = [(255,255,255)] * margin_width
         for vals in row:
-            tmp.append(next('■■ ' if val == 1 else '   ' for val in vals if val !=2))
-        out.append(''.join(tmp))
-    return '\n'.join(out)
+            val = next((0,0,0) if val == 1 else (255,255,255) for val in vals if val != 2)
+            tmp.extend([val] * (width // 25))
+        tmp.extend([(255,255,255)] * margin_width)
+        out.append(tmp * (height // 6))
+    # print(out)
+    for _ in range(margin_width):
+        out.append(margins)
+    # print(len(out), len(out[0]))
+    im = Image.new('RGB', (width + 2 * margin_width, height + 2 * margin_width))
+    im.putdata([k for row in out for k in row])
+    im.convert('1').save("day8.png")
+    proc = sp.Popen("tesseract day8.png stdout -l eng --oem 0 --psm 6", stdout=sp.PIPE, stderr=sp.PIPE)
+    out, err = proc.communicate()
+
+    os.unlink("day8.png")
+
+    return out.decode().strip()
 
 if __name__ == "__main__":
     import sys
     inp = parse(sys.stdin.read())
     print(part1(inp.copy()))
+    print("=========")
     print(part2(inp.copy()))
